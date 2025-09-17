1b7ab73f3e06a8953f74988ab733b339b735326e
Raja_Rajeshwaran_P <rajarajeshwaran2004@gmail.com>
2025-09-18 00:54:13 +0530

    Time: 3 ms (79.96%), Space: 44.4 MB (66.43%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,40 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length==0) return null;
+        Queue<ListNode> queue=new LinkedList<>();
+        for(ListNode list:lists) queue.offer(list);
+        while(queue.size()>1){
+            ListNode first=queue.poll();
+            ListNode second=queue.poll();
+            queue.offer(mergeTwoLists(first,second));
+        }
+        return queue.poll();
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head=new ListNode(0);
+        ListNode dummy=head;
+        while(list1!=null && list2!=null){
+            int data1=list1.val;
+            int data2=list2.val;
+            if(data1<data2){
+                head.next=list1;
+                list1=list1.next;
+            }
+            else if(data1>data2){
+                head.next=list2;
+                list2=list2.next;
+            }
+            else if(data1==data2){
+                 head.next=list1;
+                 list1=list1.next; 
+                 head=head.next;
+                 head.next=list2; 
+                 list2=list2.next;
+            }
+            head=head.next;
+        }
+        if(list1!=null) head.next=list1;
+        if(list2!=null) head.next=list2;
+        return dummy.next;
+    }
+}
